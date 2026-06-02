f856fa54b16db22c98ec9aa414a7c39ecbafb916
ahmedalraddad <eng.ahmedalraddad@gmail.com>
2026-06-03 02:01:23 +0300

    final:product

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,10 @@ import helmet from "helmet";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle("Cats example")
-    .setDescription("The cats API description")
+    .setTitle("team management API")
+    .setDescription("The team API description")
     .setVersion("1.0")
-    .addTag("cats")
+    .addTag("team management")
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, documentFactory);
